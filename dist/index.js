4e99809d1ed6e9f3e72c9647e601bcde3d4236dc
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-01-05 02:58:23 +0000

    enums!

--- dist/index.js
+++ dist/index.js
@@ -27,3 +27,28 @@ employee = [
 let pid;
 pid = 22;
 pid = "22";
+// Enum
+// these have values 0, 1, 2, 3 by default
+// can change the initial value (to start from 5 instead of 0)
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 5] = "Up";
+    Direction1[Direction1["Down"] = 6] = "Down";
+    Direction1[Direction1["Left"] = 7] = "Left";
+    Direction1[Direction1["Right"] = 8] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log("Direction1 - Up is: ", Direction1.Up);
+console.log("Direction1 - Down is: ", Direction1.Down);
+console.log("Direction1 - Left is: ", Direction1.Left);
+console.log("Direction1 - Right is: ", Direction1.Right);
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log("Direction2 - Up is: ", Direction2.Up);
+console.log("Direction2 - Down is: ", Direction2.Down);
+console.log("Direction2 - Left is: ", Direction2.Left);
+console.log("Direction2 - Right is: ", Direction2.Right);
--- src/index.ts
+++ src/index.ts
@@ -35,3 +35,30 @@ let pid: string | number;
 
 pid = 22;
 pid = "22";
+
+// Enum
+// these have values 0, 1, 2, 3 by default
+// can change the initial value (to start from 5 instead of 0)
+enum Direction1 {
+  Up = 5,
+  Down,
+  Left,
+  Right,
+}
+
+console.log("Direction1 - Up is: ", Direction1.Up);
+console.log("Direction1 - Down is: ", Direction1.Down);
+console.log("Direction1 - Left is: ", Direction1.Left);
+console.log("Direction1 - Right is: ", Direction1.Right);
+
+enum Direction2 {
+  Up = "Up",
+  Down = "Down",
+  Left = "Left",
+  Right = "Right",
+}
+
+console.log("Direction2 - Up is: ", Direction2.Up);
+console.log("Direction2 - Down is: ", Direction2.Down);
+console.log("Direction2 - Left is: ", Direction2.Left);
+console.log("Direction2 - Right is: ", Direction2.Right);
