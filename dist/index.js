e3165d9be8a5894a91699963fc0f59ae190b8575
Elena Marinaki <76445935+elenamarinaki@users.noreply.github.com>
2022-01-05 02:48:37 +0000

    more types - tuples

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,19 @@
 "use strict";
 let id = 5;
+let company = "media company";
+let isPublished = true;
+// assign any type
+let x = "hello";
+let ids = [1, 2, 3, 4, 5];
+x = 5;
+x = "elena";
 // error!
 // id = "5";
 id = 7;
 id = id + 1;
+console.log("ID is: ", id);
+// error
+// ids.push("marika");
+ids.push(6);
+// Tuple
+let person = [1, "Elena", true];
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 let id: number = 5;
+let company: string = "media company";
+let isPublished: boolean = true;
+// assign any type
+let x: any = "hello";
+let ids: number[] = [1, 2, 3, 4, 5];
+
+x = 5;
+x = "elena";
 
 // error!
 // id = "5";
 
 id = 7;
 id = id + 1;
+
+console.log("ID is: ", id);
+
+// error
+// ids.push("marika");
+ids.push(6);
+
+// Tuple
+let person: [number, string, boolean] = [1, "Elena", true];
